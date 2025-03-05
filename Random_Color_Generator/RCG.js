589113eb956f3a4d602f571bfe8513b5dd5f4ea0
Yahya Qureshi <120775377+YahyaQureshii@users.noreply.github.com>
2025-03-06 00:14:48 +0500

    Add files via upload

---
+++ Random_Color_Generator/RCG.js
@@ -0,0 +1,7 @@
+console.log("initializing RCG.js");
+// console.log("initializing RCG.js");
+
+// let boxes= document.getElementsByClassName("box")
+
+let boxes= document.getElementById(".container").children
+console.log(boxes)
